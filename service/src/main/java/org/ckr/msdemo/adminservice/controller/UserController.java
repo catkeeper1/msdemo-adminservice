8299e5487512e2842d9fb21e9454ac1964223f0e
catkeeper1 <catkeeper1@gmail.com>
2017-07-10 00:15:20 +0800

    provide init data

--- service/src/main/java/org/ckr/msdemo/adminservice/controller/UserController.java
+++ service/src/main/java/org/ckr/msdemo/adminservice/controller/UserController.java
@@ -2,11 +2,16 @@ package org.ckr.msdemo.adminservice.controller;
 
 import org.ckr.msdemo.adminservice.service.UserService;
 import org.ckr.msdemo.adminservice.valueobject.UserDetailView;
+import org.ckr.msdemo.pagination.PaginationContext;
+import org.ckr.msdemo.pagination.PaginationSupported;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -22,6 +27,19 @@ public class UserController {
     public UserDetailView getUser(@PathVariable("userId") String userName) {
 
         return this.userService.getUser(userName);
+
+    }
+
+
+    @RequestMapping(value = "/user/queryUser", method = RequestMethod.GET)
+    @PaginationSupported
+    public List<UserDetailView> getUsers() {
+
+        PaginationContext.getQueryRequest();
+
+        PaginationContext.setResponseInfo(10L, 98L, 1000L);
+
+        return new ArrayList<>();
 
     }
 }
