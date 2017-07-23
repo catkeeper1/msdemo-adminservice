ee794eaaccdfa6eab0578015b67e7f74553fa8d9
catkeeper1 <catkeeper1@gmail.com>
2017-07-23 19:49:28 +0800

    adjust DB UT

--- model/src/test/java/org/ckr/msdemo/adminservice/SpringTestConfig.java
+++ model/src/test/java/org/ckr/msdemo/adminservice/SpringTestConfig.java
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * This class is only used to init spring boot container for DAO unit test case.
- * @SpringBootApplication is needed for spring container initialization.
+ * Annotation SpringBootApplication is needed for spring container initialization.
  */
 @SpringBootApplication
 public class SpringTestConfig {
