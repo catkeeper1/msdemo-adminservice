5b733ef053c4ebdedb53028d061b3ebb5051a841
catkeeper1 <catkeeper1@gmail.com>
2017-09-09 21:42:59 +0800

    Use authentication to protect url.

--- service/src/main/java/org/ckr/msdemo/adminservice/config/ProtectedResourceConfig.java
+++ service/src/main/java/org/ckr/msdemo/adminservice/config/ProtectedResourceConfig.java
@@ -1,5 +1,6 @@
 package org.ckr.msdemo.adminservice.config;
 
+import org.ckr.msdemo.authserver.cyrpto.AsymmetricVerifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -23,79 +24,43 @@ import java.security.spec.X509EncodedKeySpec;
 /**
  * Created by Administrator on 2017/9/3.
  */
-@EnableResourceServer
-@Configuration
-public class ProtectedResourceConfig extends ResourceServerConfigurerAdapter {
-
-    @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-        resources.tokenStore(new JwtTokenStore(this.jwtAccessTokenConverter()));
-    }
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
-
-
-    }
-
-
-    @Bean
-    public JwtAccessTokenConverter jwtAccessTokenConverter()  {
-
-        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-        String keyStr = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPS0uGZGdksBcNzRuLq2/6RD7gQOzZi0WhcVKCF/8ePlXg991xlOHEy7IP9ZWA+svWCVM22cts7v6hOaDoSFp7A==";
-
-        byte[] keyBytes = Base64Utils.decodeFromString(keyStr);
-        try {
-            X509EncodedKeySpec spec =
-                    new X509EncodedKeySpec(keyBytes);
-            KeyFactory kf = KeyFactory.getInstance("EC");
-            PublicKey publicKey = kf.generatePublic(spec);
-
-            converter.setVerifier(new AsymmetricVerifier(publicKey, "SHA256withECDSA"));
-        } catch (Exception ie) {
-            throw new RuntimeException("", ie);
-        }
-        return converter;
-    }
-
-    public static class AsymmetricVerifier implements SignatureVerifier {
-
-        private final PublicKey key;
-        private final String algorithm;
-
-
-        public AsymmetricVerifier(RSAPublicKey key) {
-            this(key, "SHA256withRSA");
-        }
-
-        public AsymmetricVerifier(PublicKey key, String algorithm) {
-            this.key = key;
-            this.algorithm = algorithm;
-        }
-
-
-
-        public void verify(byte[] content, byte[] sig) {
-            try {
-                Signature signature = Signature.getInstance(algorithm);
-                signature.initVerify(key);
-                signature.update(content);
-
-                if (!signature.verify(sig)) {
-                    throw new InvalidSignatureException("Signature did not match content");
-                }
-            }
-            catch (GeneralSecurityException e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public String algorithm() {
-            return algorithm;
-        }
-
-    }
-
-}
+//@EnableResourceServer
+//@Configuration
+//public class ProtectedResourceConfig extends ResourceServerConfigurerAdapter {
+//
+//    @Override
+//    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+//        resources.tokenStore(new JwtTokenStore(this.jwtAccessTokenConverter()));
+//    }
+//
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests().anyRequest().permitAll();
+//
+//
+//    }
+//
+//
+//    @Bean
+//    public JwtAccessTokenConverter jwtAccessTokenConverter()  {
+//
+//        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+//        String keyStr = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEPS0uGZGdksBcNzRuLq2/6RD7gQOzZi0WhcVKCF/8ePlXg991xlOHEy7IP9ZWA+svWCVM22cts7v6hOaDoSFp7A==";
+//
+//        byte[] keyBytes = Base64Utils.decodeFromString(keyStr);
+//        try {
+//            X509EncodedKeySpec spec =
+//                    new X509EncodedKeySpec(keyBytes);
+//            KeyFactory kf = KeyFactory.getInstance("EC");
+//            PublicKey publicKey = kf.generatePublic(spec);
+//
+//            converter.setVerifier(new AsymmetricVerifier(publicKey, "SHA256withECDSA"));
+//        } catch (Exception ie) {
+//            throw new RuntimeException("", ie);
+//        }
+//        return converter;
+//    }
+//
+//
+//
+//}
