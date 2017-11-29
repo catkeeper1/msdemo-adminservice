aba18d6f4eddc2d1b1c2cb24eecd196de75b47e3
Lin, Yukai A <yukai.a.lin@accenture.com>
2017-11-29 19:04:03 +0800

    change spring security to read ldap

--- service/src/main/java/org/ckr/msdemo/adminservice/config/WebSecurityConfig.java
+++ service/src/main/java/org/ckr/msdemo/adminservice/config/WebSecurityConfig.java
@@ -1,14 +1,16 @@
 package org.ckr.msdemo.adminservice.config;
 
-import org.ckr.msdemo.adminservice.service.SecurityEvaluatorService;
-import org.ckr.msdemo.reserver.config.EnableJWTTokenAuthentication;
-import org.ckr.msdemo.reserver.config.ResourceServerCustomizedSecurityConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.encoding.PlaintextPasswordEncoder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
 /**
@@ -16,41 +18,115 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
  */
 
 
-@Configuration
-@EnableJWTTokenAuthentication
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+//@Configuration
+//@EnableJWTTokenAuthentication
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+//public class WebSecurityConfig {
+//
+//
+//    @Bean
+//    public SecurityEvaluatorService securityEvaluatorService() {
+//        return new SecurityEvaluatorService();
+//    }
+//
+//
+//    @Bean
+//    public ResourceServerCustomizedSecurityConfigurer resourceServerSecurityConfigurer() {
+//
+//        return new ResourceServerCustomizedSecurityConfigurer() {
+//
+//            @Override
+//            public void configure(HttpSecurity http) throws Exception {
+//                http.authorizeRequests()
+//                    //for swagger UI, just by pass security checking.
+//                    .antMatchers("/swagger-ui.html",
+//                                 "/swagger-resources",
+//                                 "/swagger-resources/**/*",
+//                                 "/webjars/**/*",
+//                                 "/v2/api-docs").permitAll()
+//                    //for all url start with /pub(for public endpoint), by pass security checking.
+//                    .antMatchers("/pub/**/*").permitAll()
+//                    //for all other endpoints, cannot access until authenticated.
+//                    //if you want to disable the checking for development, please change it to permitAll().
+//                    .anyRequest().authenticated()
+//                        .and()
+//                        .formLogin().
+//
+//            }
+//        };
+//
+//
+//    }
+//
+//}
+@EnableWebSecurity
 public class WebSecurityConfig {
 
 
+//    @Autowired
+//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER");
+//    }
     @Bean
-    public SecurityEvaluatorService securityEvaluatorService() {
-        return new SecurityEvaluatorService();
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(User.withUsername("user").password("password").roles("USER").build());
+        manager.createUser(User.withUsername("user2").password("password2").roles("USER").build());
+        return manager;
     }
 
-
-    @Bean
-    public ResourceServerCustomizedSecurityConfigurer resourceServerSecurityConfigurer() {
-
-        return new ResourceServerCustomizedSecurityConfigurer() {
-
-            @Override
-            public void configure(HttpSecurity http) throws Exception {
-                http.authorizeRequests()
+    @Configuration
+    @Order(1)
+    public static class BasicWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
                     //for swagger UI, just by pass security checking.
                     .antMatchers("/swagger-ui.html",
-                                 "/swagger-resources",
-                                 "/swagger-resources/**/*",
-                                 "/webjars/**/*",
-                                 "/v2/api-docs").permitAll()
+                            "/swagger-resources",
+                            "/swagger-resources/**/*",
+                            "/webjars/**/*",
+                            "/v2/api-docs")
+//                    .permitAll()
                     //for all url start with /pub(for public endpoint), by pass security checking.
-                    .antMatchers("/pub/**/*").permitAll()
+//                    .antMatchers("/**/*")
+                    .authenticated()
                     //for all other endpoints, cannot access until authenticated.
                     //if you want to disable the checking for development, please change it to permitAll().
-                    .anyRequest().permitAll();
-            }
-        };
+                    .and().httpBasic();
+        }
 
 
+    }
+
+    @Configuration
+    @Order(2)
+    public static class FormLoginWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                    .formLogin();
+        }
+
+//        @Override
+//        public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//            auth
+//                    .ldapAuthentication()
+//                    .userDnPatterns("uid={0},ou=people")
+//                    .groupSearchBase("ou=groups")
+//                    .contextSource()
+//                    .url("ldap://localhost:8389/dc=springframework,dc=org")
+//                    .and()
+//                    .passwordCompare()
+////                    .passwordEncoder(new LdapShaPasswordEncoder())
+//                    .passwordEncoder(new PlaintextPasswordEncoder())
+//                    .passwordAttribute("userPassword");
+//        }
     }
 
 }
