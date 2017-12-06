921f209806e92903b30e707c8603d9eb3e642b37
ruoli.chen <catkeeper1@gmail.com>
2017-12-06 20:21:11 +0800

    recover the pre auth example

--- service/src/main/java/org/ckr/msdemo/adminservice/constant/FunctionPointConstant.java
+++ service/src/main/java/org/ckr/msdemo/adminservice/constant/FunctionPointConstant.java
@@ -2,5 +2,5 @@ package org.ckr.msdemo.adminservice.constant;
 
 public class FunctionPointConstant {
 
-    public static final String QUERY_ALL_USERS = "query_all_users";
+    public static final String QUERY_USERS = "query_users";
 }
